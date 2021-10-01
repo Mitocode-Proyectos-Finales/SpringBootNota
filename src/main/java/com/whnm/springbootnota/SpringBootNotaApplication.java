9f0520f4d09f81089207f4f4e2f598d6e5b63223
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2021-09-30 23:29:29 -0500

    commit inicial

---
+++ src/main/java/com/whnm/springbootnota/SpringBootNotaApplication.java
@@ -0,0 +1,13 @@
+package com.whnm.springbootnota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whnm/springbootnota/SpringBootNotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whnm.springbootnota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
